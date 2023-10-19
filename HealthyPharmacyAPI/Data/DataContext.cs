22ad747c783b85186281fc05abf062a792adf433
Hesh-Kavinda <heshan@ascnetic.se>
2023-10-19 12:40:01 +0530

    intial commit

---
+++ HealthyPharmacyAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthyPharmacyAPI.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options): base(options)
+		{
+
+		}
+
+		public DbSet<Character> Characters => Set<Character>();
+	}
+}
+
---
+++ HealthyPharmacyAPI/Models/Character.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace HealthyPharmacyAPI.Models
+{
+	public class Character
+	{
+		public int Id { get; set; }
+	}
+}
+
